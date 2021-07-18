1bba910de95b07fa25029e5baf3810e67c6eb958
nag.rotte@gmail.com <nag.rotte@gmail.com>
2021-07-17 21:36:04 -0500

    basket.api checking in main coding is complete with repository , Controller and Services configured

---
+++ amethyst-micro-src/Services/Basket/basket.api/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using basket.api.Entities;
+using System.Threading.Tasks;
+
+namespace basket.api.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
