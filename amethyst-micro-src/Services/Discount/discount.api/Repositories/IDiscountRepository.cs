39a71b8dd5e90f5f0f13c7bacc61dd390a4225cd
nag.rotte@gmail.com <nag.rotte@gmail.com>
2021-07-18 11:01:26 -0500

    Dicuount Microservices with Postgres including Dockerization is complete

---
+++ amethyst-micro-src/Services/Discount/discount.api/Repositories/IDiscountRepository.cs
@@ -0,0 +1,14 @@
+﻿using discount.api.Entities;
+using System.Threading.Tasks;
+
+namespace discount.api.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
